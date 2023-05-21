bf0c3f3c1099e36323c2c3a5158d60a50dcf7276
mikiemapo <123407631+mikiemapo@users.noreply.github.com>
2023-05-21 00:11:38 +0000

    front end login page setup ran debug use state from {} to [] debug session 1 e to fix jsonfy e

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -5,43 +5,42 @@ import "../../styles/home.css";
 
 export const Login = () => {
 	const { store, actions } = useContext(Context);
-	const [email, setEmail] = useState(" ");
-	const [password, setPassword] = useState(" ");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
 
 	const handleClick = () => {
 		const opts = {
 			method: 'POST',
-			Body: JSON.stringify({
-				"email": "test" ,
- 				"password" : "test"
-		})
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({
+				email: email,
+				password: password
+			})
 		};
 
-
 		fetch('https://mikiemapo-sturdy-enigma-56p47gpvj79375jp-3001.preview.app.github.dev/api/token', opts)
-		.then(resp =>{
-			if(resp.status == 200) return resp.json;
-			else alert('THERE WAS AN ERROR!!!!!!!!');
-		})
-		.then()
-		.catch(error => {
-			console.error('THERE WAS AN ERROR!!!!!!!!', error);
-		})
+			.then(resp => {
+				if (resp.status === 200) {
+					return resp.json();
+				} else {
+					alert('THERE WAS AN ERROR!!!!!!!!');
+				}
+			})
+			.then(data => {
+				// Handle the response data here
+			})
+			.catch(error => {
+				console.error('THERE WAS AN ERROR!!!!!!!!', error);
+			});
 	}
 
-	
 	return (
 		<div className="text-center mt-5">
 			<h1>Login</h1>
-			
-			
-				<input type='text' placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-                
-                <input type='password' placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
-				
-                <button onClick={handleClick}>LOGIN</button>
-                
-			
+
+			<input type='text' placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+			<input type='password' placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+			<button onClick={handleClick}>LOGIN</button>
 		</div>
 	);
 };
