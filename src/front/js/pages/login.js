d87f30bf83614b257112ecbf3e432a23aa760823
mikiemapo <123407631+mikiemapo@users.noreply.github.com>
2023-05-23 01:20:33 +0000

    set login to store and used context hook  for refresh override

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -1,68 +1,50 @@
 import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
+import { useHistory } from "react-router-dom";
 import "../../styles/home.css";
 import { Link } from "react-router-dom";
+import { Router } from "@reach/router";
 
 export const Login = () => {
 	const { store, actions } = useContext(Context);
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
-	const token = sessionStorage.getItem('token');
+	//const history = useHistory();
 
 	// LOG OUT !!
 	const handleOut = () => {
-		sessionStorage.clear()
-	}
+		sessionStorage.clear();
+	};
 
 	// LOG IN !!
+	console.log("this is your token", store.token)
 	const handleClick = () => {
-		const opts = {
-			method: 'POST',
-			headers: { "Content-Type": "application/json" },
-			body: JSON.stringify({
-				email: email,
-				password: password
-			})
-		};
-
-
-		fetch('https://mikiemapo-sturdy-enigma-56p47gpvj79375jp-3001.preview.app.github.dev/api/token', opts)
-			.then(resp => {
-				if (resp.status === 200) {
-					return resp.json();
-				} else {
-					alert('THERE WAS AN ERROR!!!!!!!!');
-				}
-			})
-			.then(data => {
-				console.log('ACCESSTOKENBACK', data)
-				sessionStorage.setItem('token', data.access_token);
-			})
-			.catch(error => {
-				console.error('THERE WAS AN ERROR!!!!!!!!', error);
-			});
-	}
-
+		actions.login(email, password).then();
+	};
+	//if (store.token && store.token != "" && store.token != undefined) history.push("/");
 	return (
 		<div className="text-center mt-5">
 			<h1>Login</h1>
-			{
-			(token && token!="" && token!= undefined) 
-			? 
-			<div>
-			'You are logged in' + {token} 
-				<Link to ='/login' >
-				<button onClick={handleOut}>SIGN OUT</button>
-				</Link>
-			</div> 
-			: 
-			<div>
-			<input type='text' placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-			<input type='password' placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-			<button onClick={handleClick}>LOGIN</button>
-			</div>
-			}
+			{store.token && store.token !== "" && store.token !== undefined ? (
+				<div>
+					"You are logged in" + {store.token}
+					<Link to="/login">
+						<button onClick={handleOut}>SIGN OUT</button>
+					</Link>
+				</div>
+			) : (
+				<div>
+					<input type="text" placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+					<input
+						type="password"
+						placeholder="password"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+					<button onClick={handleClick}>LOGIN</button>
+				</div>
+			)}
 		</div>
 	);
 };
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,6 +1,7 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
+			token: null,
 			message: null,
 			demo: [
 				{
@@ -16,35 +17,54 @@ const getState = ({ getStore, getActions, setStore }) => {
 			]
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
 			exampleFunction: () => {
 				getActions().changeColor(0, "green");
 			},
 
-			getMessage: async () => {
-				try{
-					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
-					const data = await resp.json()
-					setStore({ message: data.message })
-					// don't forget to return something, that is how the async resolves
-					return data;
-				}catch(error){
-					console.log("Error loading message from backend", error)
+			login: async (email, password) => {
+				try {
+					const opts = {
+						method: 'POST',
+						headers: { "Content-Type": "application/json" },
+						body: JSON.stringify({
+							email: email,
+							password: password
+						})
+					};
+
+					const resp = await fetch('https://mikiemapo-sturdy-enigma-56p47gpvj79375jp-3001.preview.app.github.dev/api/token', opts);
+					if (resp.status !== 200) {
+						alert('THERE WAS AN ERROR!!!!!!!!');
+						return false;
+					}
+
+					const data = await resp.json();
+					console.log('ACCESSTOKENBACK', data);
+					sessionStorage.setItem('token', data.access_token);
+					setStore({ token: data.access_token });
+					return true;
+				} catch (error) {
+					console.error('THERE WAS AN ERROR!!!!!!!!', error);
 				}
 			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
+			getMessage: async () => {
+				try {
+					const resp = await fetch(process.env.BACKEND_URL + "/api/hello");
+					const data = await resp.json();
+					setStore({ message: data.message });
+					return data;
+				} catch (error) {
+					console.log("Error loading message from backend", error);
+				}
+			},
+
+			changeColor: (index, color) => {
+				const store = getStore();
 				const demo = store.demo.map((elm, i) => {
 					if (i === index) elm.background = color;
 					return elm;
 				});
-
-				//reset the global store
 				setStore({ demo: demo });
 			}
 		}
